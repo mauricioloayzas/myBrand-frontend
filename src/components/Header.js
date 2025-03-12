ccf3f4cdc5bc846ef4e5a15f898d952400d0da48
macUser <macuser@macUsers-MacBook-Pro.local>
2025-03-11 23:56:06 -0500

    Set the firebase configuration to get the information from firebase cloud firestore

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,45 @@ import React, { useEffect, useState } from "react";
 import LineIcon from "react-lineicons";
 import { Link, NavLink } from "react-router-dom";
 import { Image } from "./common/Image";
+import { db } from "../firebase";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 function Header() {
   const [information, setInformation] = useState("");
   const [navigationToggler, setNavigationToggler] = useState(false);
+  const appId = process.env.REACT_APP_APP_IDENTIFIER;
 
   const handleNavigationToggler = () => {
     setNavigationToggler(!navigationToggler);
   };
 
   useEffect(() => {
-    axios.get("/api/information").then((response) => {
-      setInformation(response.data);
-    });
+    //axios.get("/api/information").then((response) => {
+      //setInformation(response.data);
+    //});
+
+    const fetchData = async () => {
+      try {
+        const informationsRef = collection(
+            db,
+            "Apps/"+appId+"/Informations"
+        );
+        const querySnapshot = await getDocs(informationsRef);
+
+        if (!querySnapshot.empty) {
+          const docData = querySnapshot.docs[0].data(); // Get the first matching document
+          setInformation(docData);
+        } else {
+          console.log("Apps/"+appId+"/Informations");
+          console.log("No document found with app_id:", appId);
+        }
+      } catch (error) {
+        console.error("appIdentifier:", appId);
+        console.error("Error fetching Firestore document:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
@@ -31,7 +57,7 @@ function Header() {
         <div className="mi-header-image">
           <Link to="/">
             <Image
-              src={information.brandImage}
+              src={information.brand_image}
               placeholder="/images/about-image-placeholder.png"
               alt="brandimage"
             />
@@ -75,10 +101,9 @@ function Header() {
           <b>
             <a
               rel="noopener noreferrer"
-              target="_blank"
-              href="https://nuclearthemes.com"
+              href="#"
             >
-              NuclearThemes
+              Mauloasan.
             </a>
           </b>
         </p>
